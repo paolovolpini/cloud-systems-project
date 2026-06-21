720cc8516516a9f144737c194789b015c34b924c
paolovolpini <paolo.volpini4@gmail.com>
2026-06-21 10:37:57 +0200

    Modifying tf sources to include auto scaling and ecr full deletion

--- infrastructure/opentofu/variables.tf
+++ infrastructure/opentofu/variables.tf
@@ -6,7 +6,6 @@ variable "control_plane_instance_type" { default = "t3.small" }
 variable "worker_instance_type"        { default = "t3.small" }
 variable "worker_min_size"             { default = 2 }
 variable "worker_max_size"             { default = 4 }
-variable "worker_desired"              { default = 2 }
 
 # ubuntu 26.04 in south-1
 variable "ami_id" { default = "ami-09420cfad91ebef79" }
